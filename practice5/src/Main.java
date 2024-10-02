2a4879e550ac8daa940d5a5240e4001d4eea5173
Akhat <zhumanov_ahat00@mail.ru>
2024-10-02 14:41:37 +0500

    modified practice 5

--- practice5/src/Main.java
+++ practice5/src/Main.java
@@ -1,22 +1,34 @@
 import model.*;
 import modelCreator.*;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        DocumentCreator reportCreator = new ReportCreator();
-        Document report = reportCreator.CreateDocument();
-        report.open();
-
-        DocumentCreator resumeCreator = new ResumeCreator();
-        Document resume = resumeCreator.CreateDocument();
-        resume.open();
-
-        DocumentCreator letterCreator = new LetterCreator();
-        Document letter = letterCreator.CreateDocument();
-        letter.open();
-
-        DocumentCreator invoiceCreator = new InvoiceCreator();
-        Document invoice = invoiceCreator.CreateDocument();
-        invoice.open();
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Выберите тип файла: ");
+        String choice = scan.next().toLowerCase();
+        switch (choice) {
+            case "report":
+                DocumentCreator reportCreator = new ReportCreator();
+                Document report = reportCreator.CreateDocument();
+                report.open();
+                break;
+            case "resume":
+                DocumentCreator resumeCreator = new ResumeCreator();
+                Document resume = resumeCreator.CreateDocument();
+                resume.open();
+                break;
+            case "letter":
+                DocumentCreator letterCreator = new LetterCreator();
+                Document letter = letterCreator.CreateDocument();
+                letter.open();
+            case "invoice":
+                DocumentCreator invoiceCreator = new InvoiceCreator();
+                Document invoice = invoiceCreator.CreateDocument();
+                invoice.open();
+            default:
+                throw new IllegalStateException("Неподходящий тип файла: " + choice);
+        }
     }
 }
